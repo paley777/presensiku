2eaff439d42eb5692c14a0ade48cf598e10fa1fd
Valleryan Virgil Zuliuskandar <valleryan1212@gmail.com>
2022-11-09 00:46:18 +0700

    Items #7

---
+++ database/migrations/2022_11_08_114405_create_presensis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presensis', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_presensi');
+            $table->string('id_user');
+            $table->string('id_mapel');
+            $table->string('id_kelas');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presensis');
+    }
+};
---
+++ database/migrations/2022_11_08_150655_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_presensi');
+            $table->string('nama_presensi');
+            $table->string('id_siswa');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+};
